30a0af17c5eddf9539e31959fea091d8d894c0f4
KonstantinShirmanov <45903009+KonstantinShirmanov@users.noreply.github.com>
2020-06-22 19:13:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ function calculate (){
 	var ops2 = document.getElementById("opList2").options[document.getElementById("opList2").options.selectedIndex].text;
 	var ops3 = document.getElementById("opList3").options[document.getElementById("opList3").options.selectedIndex].text;
 
-	var a = Number.parseDouble(astr);
-	var b = Number.parseDouble(bstr);
-	var c = Number.parseDouble(cstr);
+	var a = Number.parseFloat(astr);
+	var b = Number.parseFloat(bstr);
+	var c = Number.parseFloat(cstr);
 
 	console.log()
 	var res = 0;
