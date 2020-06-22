75db7c882e624fc887d00c8d5ff1a32b8bc0f424
KonstantinShirmanov <45903009+KonstantinShirmanov@users.noreply.github.com>
2020-06-22 17:10:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,6 @@
 function calculate(){
 	console.log("run calc");
-	var a = +document.getElementById("first").text;
-	var b = +document.getElementById("second").text;
-	var c = +document.getElementById("third").text;
-	var ops1 = document.getElementById("opList1").options[document.getElementById("opList1").options.selectedIndex].text;
-	var ops2 = document.getElementById("opList2").options[document.getElementById("opList2").options.selectedIndex].text;
-	var ops3 = document.getElementById("opList3").options[document.getElementById("opList3").options.selectedIndex].text;
-
-	var res = 0;
-
-	if (ops1 == "+") a = +document.getElementById("first").text;
-	else if (ops1=="-") a = -document.getElementById("first").text;
-	if (ops2 =="+"){
-	b = +document.getElementById("second").text;
-	res += (a+b);
-}
-	else if (ops2 =="-"){
-	b = +document.getElementById("second").text;
-	res += (a-b);
-}
-
-
-	else if (ops2 =="/"){
-	if (b!=0)
-	res += (a/b);
-	else {alert ("Нельзя делить на 0");}
-	}
-
-	else if (ops2 =="*"){
-	b = +document.getElementById("second").text;
-	res += (a*b);
-	}
-
-	if (ops3 =="+"){
-	res += (res+c);
-}
-	else if (ops3 =="-"){
-	res += (res-c);
-}
-
-
-	else if (ops3 =="/"){
-	if (c!=0)
-	res += (res/c);
-	else {alert ("Нельзя делить на 0");}
-	}
-
-	else if (ops3 =="*"){
 	
-	res += (res*c);
-	}
-
-	document.getElementById(result).text = ""+res;
-}
-
 function clear (){
 	document.getElementById("first").value =null;
 	document.getElementById("second").value = null;
