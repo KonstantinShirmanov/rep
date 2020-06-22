c82b899de553914de3f04e329e695964aba757df
KonstantinShirmanov <45903009+KonstantinShirmanov@users.noreply.github.com>
2020-06-22 16:38:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function calculate(){
 	var a = +document.getElementById("first").text;
 	var b = +document.getElementById("second").text;
 	var c = +document.getElementById("third").text;
-	var ops = document.getElementByTegName("operation");
 	var ops1 = document.getElementById("opList1").options[document.getElementById("opList1").options.selectedIndex].text;
 	var ops2 = document.getElementById("opList2").options[document.getElementById("opList2").options.selectedIndex].text;
 	var ops3 = document.getElementById("opList3").options[document.getElementById("opList3").options.selectedIndex].text;
